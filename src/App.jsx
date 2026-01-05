1146db901922872ddcf4db33dcac3d75a5f14fb6
kaungkhantcoder <kaungkhantcoder@gmail.com>
2026-01-05 23:41:38 +0630

    route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Buttons from "./components/Services";
 import Profile from "./components/Profile";
 import Payment from "./components/Paymet";
 import History from "./components/History";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Greeting = () => {
   const hour = new Date().getHours();
@@ -17,6 +18,9 @@ export default function App() {
 
   return (
     <>
+    <BrowserRouter>
+    <Routes></Routes>
+    </BrowserRouter>
     <div className="w-[375px] h-auto mx-auto m-5 p-5 border-[5px] shadow-md border-black rounded-[30px]">
       <h1 className="text-orange-500 font-bold">{Greeting()}</h1>
       <Profile />
