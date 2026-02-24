825152e6ebd0fc65c907ad3994716a3851589c35
kaungkhantcoder <kaungkhantcoder@gmail.com>
2026-02-24 22:53:57 +0630

    connect signin front and back

--- frontend/src/pages/auth/SignIn.jsx
+++ frontend/src/pages/auth/SignIn.jsx
@@ -1,86 +1,81 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios"; // axios
 
 export default function SignIn() {
   const navigate = useNavigate();
+  // State
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/login", {
+        email: email,
+        password: password
+      });
+
+      if (response.status === 200) {
+        //  Store token in the LocalStorage
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("username", response.data.username);
+        alert("Successful Login");
+        navigate("/dashboard"); // Dashboard
+      }
+    } catch (error) {
+      alert(error.response?.data?.message || "Login Error");
+    }
+  };
 
   return (
     <>
-      <div class="flex min-h-full flex-col  px-6 py-12">
-        <button className="flex items-center justify-start h-10 ">
-          <h3
-            className="bg-orange-500 py-2 px-3 text-white  rounded-md"
-            onClick={() => navigate("/")}
-          >
-            Back
-          </h3>
+      <div className="flex min-h-full flex-col px-6 py-12">
+        {/* Back Button */}
+        <button className="flex items-center justify-start h-10 " onClick={() => navigate("/")}>
+          <h3 className="bg-orange-500 py-2 px-3 text-white rounded-md">Back</h3>
         </button>
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight">
-            Sign in to your account
-          </h2>
+
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold tracking-tight">Sign in to your account</h2>
         </div>
 
-        <div class="mt-10">
-          <form action="#" method="POST" class="space-y-6">
+        <div className="mt-10">
+          <form className="space-y-6" onSubmit={handleLogin}>
             <div>
-              <label for="phone" class="block text-sm/6 font-medium">
-                Phone Number
-              </label>
-              <div class="mt-2">
+              <label className="block text-sm font-medium">Email address</label>
+              <div className="mt-2">
                 <input
-                  id="phone"
-                  type="number"
-                  name="phone"
+                  type="email"
                   required
-                  autocomplete="phone"
-                  class="block w-full rounded-md border border-orange-400  px-3 py-1.5 text-black focus:outline-none focus:border-2 focus:border-orange-500  placeholder:text-gray-500"
+                  className="block w-full rounded-md border border-orange-400 px-3 py-1.5 text-black focus:outline-none focus:border-2 focus:border-orange-500"
+                  onChange={(e) => setEmail(e.target.value)} // Email
                 />
               </div>
             </div>
 
             <div>
-              <div class="flex items-center justify-between">
-                <label for="password" class="block text-sm/6 font-medium">
-                  Password
-                </label>
-                <div class="text-sm">
-                  <a
-                    href="#"
-                    class="font-semibold text-orange-400 hover:text-orange-300"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div class="mt-2">
+              <label className="block text-sm font-medium">Password</label>
+              <div className="mt-2">
                 <input
-                  id="password"
                   type="password"
-                  name="password"
                   required
-                  autocomplete="current-password"
-                  class="block w-full rounded-md border border-orange-400  px-3 py-1.5 text-black focus:outline-none focus:border-2 focus:border-orange-500  placeholder:text-gray-500"
+                  className="block w-full rounded-md border border-orange-400 px-3 py-1.5 text-black focus:outline-none focus:border-2 focus:border-orange-500"
+                  onChange={(e) => setPassword(e.target.value)} // Password
                 />
               </div>
             </div>
 
-            <div className="">
-              <button
-                type="submit"
-                class="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-orange-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-              >
+            <div>
+              <button type="submit" className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold text-white hover:bg-orange-400">
                 Sign in
-              </button>
-
-              <button
-                type="submit"
-                class="flex w-full justify-center rounded-md  px-3 py-1.5 text-sm/6 font-semibold text-orange-500 underline"
-                onClick={() => navigate("/register")}
-              >
-                Register your account
               </button>
             </div>
           </form>
+          {/* Register link button */}
+          <button className="flex w-full justify-center mt-2 text-sm text-orange-500 underline" onClick={() => navigate("/signup")}>
+            Register your account
+          </button>
         </div>
       </div>
     </>
